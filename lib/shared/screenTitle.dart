98ba81fadc1cfe3ebfe690490f65db60519614c8
AnishCodes <anishhazra108@gmail.com>
2025-04-12 13:52:01 +0530

    lesson 2

--- lib/shared/screenTitle.dart
+++ lib/shared/screenTitle.dart
@@ -7,10 +7,26 @@ class ScreenTitle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      text,
-      style: const TextStyle(
-          fontSize: 36, color: Colors.white, fontWeight: FontWeight.bold),
+    return TweenAnimationBuilder(
+      duration: const Duration(milliseconds: 500),
+      tween: Tween<double>(begin: 0, end: 1),
+      builder: (context, value, child) {
+        return Opacity(
+          opacity: value,
+          child: Padding(
+            padding: EdgeInsets.only(top: value * 20),
+            child: child,
+          ),
+        );
+      },
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 36,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
     );
   }
 }
